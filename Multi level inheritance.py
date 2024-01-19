e0ec59761ba4fa7beeff13b1537d3e947fd8d95f
D. Naga Phanindra <104689319+Phani-LP@users.noreply.github.com>
2024-01-19 13:19:53 +0530

    Example

--- Multi level inheritance.py
+++ Multi level inheritance.py
@@ -1,3 +1,4 @@
+#Multi level inheritance
 class GrandParent:
   def method1(self):
     prinit("I am Grand Parent")
@@ -6,6 +7,15 @@ class Parent(GrandParent):
   def method2(self):
     print("I am Parent")
 
-class Child:
+class Child(Parent):
   def method3(self):
     print("I am Child")
+
+obj = Child()
+obj.method3()
+obj.method2()
+obj.method1()
+
+obj2 = Parent()
+obj2.method2()
+obj2.method1()
